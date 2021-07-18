da54e662287915320cfd6b111ae3d119d3544844
Brent2 <brents@patriot1tech.com>
2021-07-18 15:56:17 -0300

    Updating section II - cycles

--- www/src/docs/section2.ts
+++ www/src/docs/section2.ts
@@ -9,7 +9,7 @@ export const section2: Item[] = [
         id: "item1",
         name: "Cycles",
         selected: true,
-        content: html`<p>There are two cycles schools should consider when planning: the Plan, Do, Check, Act (PDCA) and annual/multi-year cycles.</p>
+        content: html`<p>There are two cycles schools should consider when planning including: the Plan, Do, Check, Act (PDCA) and annual/multi-year cycles.</p>
             <p>Long-term cycles can include annual and/or multi-year goals.</p>`,
         children: section2Cycles,
     },
