dcbd97cc7ea6b1691f4d186eded2dcebb49e11d2
Brent2 <brents@patriot1tech.com>
2021-08-23 21:11:55 -0300

    Updating appendix F

--- www/scripts/links.ts
+++ www/scripts/links.ts
@@ -62,7 +62,7 @@ export const linksObj = [
 	},
 	{
 		key: "LINK_16",
-		href: `<a href="https://collabe.nbed.nb.ca/sites/10year/nbsip/Shared%20Documents/2.%20Provincial%20Improvement%20Framework%20and%20Associated%20Documents/Annual%20School%20Performance%20Report%20FINAL.docx?Web=1" target="_blank"><strong>Appendix F</strong>: Annual School Performance Report Template</a>`,
+		href: `<a href="https://collabe.nbed.nb.ca/sites/10year/nbsip/Shared%20Documents/2.%20Provincial%20Improvement%20Framework%20and%20Associated%20Documents/School%20Self-assessment%20Documents/Annual%20School%20Performance%20Report%20Template.docx?Web=1" target="_blank"><strong>Appendix F</strong>: Annual School Performance Report Template</a>`,
 	},
 	{
 		key: "LINK_17",
